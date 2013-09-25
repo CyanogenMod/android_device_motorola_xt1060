b0d23d6c12ec64325b573a31bce8e9d1c914a719
dhacker29 <davidhackerdvm@gmail.com>
2013-09-25 02:01:04 -0500

    Fix typo

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Moto X -XT1060"
+#define BTM_DEF_LOCAL_NAME   "Moto X - XT1060"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
 #define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
